6feb7bbd9a4ae843613c0dcae16f08a8fbd3ec86
Junior Zadith Sierra Vasquez <jsierra_pr@sefin.gob.hn>
2021-06-25 11:00:43 -0600

    fist commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TablaComponent } from './components/dashboard/tabla/tabla.component';
+import { ListaComponent } from './components/dashboard/lista/lista.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    FooterComponent,
+    TablaComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule
